043b875c03a20d70417e9301790d439d99a3ec8e
Tirupati Ramrao Jamdhade <148617943+Tirupati2005@users.noreply.github.com>
2023-12-14 19:10:41 +0530

    Update urls.py

--- EcommereceWebsite/store/urls.py
+++ EcommereceWebsite/store/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from .views import ProductListCreateAPIView, OrderListCreateAPIView
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('basepage/', views.basepage, name='basepage'),
@@ -11,4 +14,6 @@ urlpatterns = [
     path('edit/<int:id>', views.edit_product, name='edit'),
     path('delete/<int:id>/', views.delete_product, name='delete'),
     path('order', views.order_product, name='order'),
+    path('products/', ProductListCreateAPIView.as_view(), name='product-list'),
+    path('orders/', OrderListCreateAPIView.as_view(), name='order-list'),
 ]
